19febcafe3ec9f01e97b5d4a9ec49e731524312e
Zapupa <vitaliy2005rus93@gmail.com>
2024-01-27 11:13:39 +0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,19 @@
 let x1, x2;
 
-const fi = (1 + Math.sqrt(5)) / 2;
-
 function fn(x) {
-  return Math.pow(x - 1, 2);
+  return x*x-4;
 }
 
 function goldenRatio() {
   var a = parseInt(document.getElementById("ainput").value);
   var b = parseInt(document.getElementById("binput").value);
 
-  while (Math.abs(b - a) > 0.00001) {
-    x1 = (b - (b - a)) / fi;
-    x2 = (a - (b - a)) / fi;
+  var fi =  (1 + Math.sqrt(5)) *0.5;
 
-    x1 > x2 ? (a = x1) : (b = x2);
-
+  while (Math.abs(b - a) > 0.0001) {
+    x1 = b - (b - a) / fi;
+    x2 = a + (b - a) / fi;
+    
     if (fn(x1) >= fn(x2)) {
       a = x1;
     } else {
@@ -30,9 +28,10 @@ function dichotomy() {
   var a = parseInt(document.getElementById("ainput").value);
   var b = parseInt(document.getElementById("binput").value);
   var c;
+  
   while (Math.abs(b - a) > 0.0001) {
     c = (a + b) / 2;
-    // alert(c);
+    
     if (fn(b) * fn(c) <= 0) {
       a = c;
     } else {
